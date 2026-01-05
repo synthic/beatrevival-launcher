1165e738ed01844a4cfc03e9d72a00a306143f87
Alex Abbatiello <50689733+synthic@users.noreply.github.com>
2026-01-04 21:20:42 -0600

    Refactor and add auth code patch

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-﻿#include <wx/wx.h>
+﻿#include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <wx/wx.h>
 
 #include "ui/ui.h"
 #include "ui/res/black.png.h"
@@ -19,47 +22,70 @@ bool App::OnInit() {
 	return true;
 }
 
+std::string ReadJson() {
+	using json = nlohmann::json;
+
+	std::ifstream f("user.json");
+
+	try {
+		json data = json::parse(f);
+		return data["authCode"];
+	} catch (json::exception) {
+		return std::string();
+	}
+}
+
 void MainWindow::ApplyPatches(wxCommandEvent& event) {
 	HWND win = FindWindowW(NULL, L"Mirror's Edge™ Catalyst");
 
-	bool patchFailed = false;
+	std::string newAuthCode = ReadJson();
 
 	if (win) {
 		DWORD pid;
 		GetWindowThreadProcessId(win, &pid);
-		HANDLE hProc = OpenProcess(PROCESS_VM_WRITE|PROCESS_VM_OPERATION, FALSE, pid);
+		HANDLE hProc = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, pid);
 
 		if (hProc) {
-			BYTE newData1[29] = "localhost";
-			BYTE newData2[] = { 0x31 };
-			BYTE newData3[] = { 0xE9, 0xC7, 0x00 };
+			BYTE hostname[29] = "localhost";
 
-			// Update redirector hostname
-			if (WriteProcessMemory(hProc, (LPVOID)0x141D80890, &newData1, (DWORD)sizeof(newData1), NULL)) {
-				SetStatusText("Success!");
-			} else {
-				SetStatusText("ERROR: WriteProcessMemory failed.");
-				connectButton->SetBitmap(black_png_to_wx_bitmap());
-				patchFailed = true;
-			}
+			BYTE newData1[] = { 0x31 };
+			BYTE newData2[] = { 0xE9, 0xC7, 0x00 };
+			BYTE newData3[] = { 0x15, 0x28, 0x04, 0x3B, 0xFE };
+			BYTE newData4[] = { 0x40, 0xB3, 0xC3, 0x41, 0x01 };
 
-			// Disable TLS in Connect function
-			if (WriteProcessMemory(hProc, (LPVOID)0x142DBE9B0, &newData2, (DWORD)sizeof(newData2), NULL) && !patchFailed) {
-				SetStatusText("Success!");
-			} else {
-				SetStatusText("ERROR: WriteProcessMemory failed.");
-				connectButton->SetBitmap(black_png_to_wx_bitmap());
-				patchFailed = true;
-			}
+			try {
+				// Update redirector hostname
+				WriteProcessMemory(hProc, (LPVOID)0x141D80890, &hostname, (DWORD)sizeof(hostname), NULL);
 
-			// Bypass encryption of authenticated requests
-			if (WriteProcessMemory(hProc, (LPVOID)0x1439C0D81, &newData3, (DWORD)sizeof(newData3), NULL) && !patchFailed) {
+				// Disable TLS in Connect function
+				WriteProcessMemory(hProc, (LPVOID)0x142DBE9B0, &newData1, (DWORD)sizeof(newData1), NULL);
+
+				// Bypass encryption of authenticated requests
+				WriteProcessMemory(hProc, (LPVOID)0x1439C0D81, &newData2, (DWORD)sizeof(newData2), NULL);
+
 				SetStatusText("Success!");
 				connectButton->SetBitmap(red_png_to_wx_bitmap());
-			} else {
+			} catch (...) {
 				SetStatusText("ERROR: WriteProcessMemory failed.");
 				connectButton->SetBitmap(black_png_to_wx_bitmap());
-				patchFailed = true;
+			}
+
+			// Update authorization code
+			if (!newAuthCode.empty()) {
+				try {
+					// Point to fixed memory address
+					WriteProcessMemory(hProc, (LPVOID)0x14388AED3, &newData3, (DWORD)sizeof(newData3), NULL);
+
+					// Write pointer
+					WriteProcessMemory(hProc, (LPVOID)0x141C3B300, &newData4, (DWORD)sizeof(newData4), NULL);
+
+					// Write new auth code
+					WriteProcessMemory(hProc, (LPVOID)0x141C3B340, newAuthCode.c_str(), newAuthCode.length(), NULL);
+
+					SetStatusText("AUTH", 1);
+				} catch (...) {
+					SetStatusText("", 1);
+				}
 			}
 
 			CloseHandle(hProc);
